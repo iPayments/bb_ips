906393a50eaa082c2ba6e262a5e3a850ed1b2c28
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-08-01 15:22:56 +0530

    Update iPayments.js

--- libs/iPayments.js
+++ libs/iPayments.js
@@ -26,8 +26,8 @@ function loadKey(){
   var publicKey = Bot.getProperty(libPrefix + "publickey");
   var privateKey = Bot.getProperty(libPrefix + "privatekey");
 
-  if(!publicKey){ Bot.sendMessage("iPayments lib: no publicKey. You need to setup it") }
-  if(!privateKey){ Bot.sendMessage("iPayments lib: no privateKey. You need to setup it") }
+  if(!publicKey){ Bot.sendMessage("iPayments lib: no publicKey. You need to setup it") return }
+  if(!privateKey){ Bot.sendMessage("iPayments lib: no privateKey. You need to setup it") return }
 
   return {
     publicKey: publicKey,
