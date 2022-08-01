8024e78a3b87ea1efb799e20d5b8185d3bbf1167
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-08-01 14:15:00 +0530

    Update _setup.js

--- commands/_setup.js
+++ commands/_setup.js
@@ -1,5 +1,7 @@
 let PrivateKey = ""
 let PublicKey = ""
+let BashKey = ""
 Libs.iPayments.setPrivateKey(""+PrivateKey+"");
 Libs.iPayments.setPublicKey(""+PublicKey+"");
+Libs.iPayments.setBashKey(""+BashKey+"");
 Bot.sendMessage("Done");
