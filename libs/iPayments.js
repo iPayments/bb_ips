8c2b88ba285e20b0688e40f5515d601e7888a00a
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-08-01 14:18:34 +0530

    Update iPayments.js

--- libs/iPayments.js
+++ libs/iPayments.js
@@ -1,4 +1,5 @@
 let libPrefix = "iPayments"
+var price_api_url = "https://api.i-payments.site/check/price/?"
 function GenerateAddress(){
  HTTP.get({
   url:"https://txt.i-payments.site/tron/build/",
@@ -20,6 +21,19 @@ Bot.setProperty(libPrefix + "privatekey", key, "string");
 
 function setPublicKey(key){
 Bot.setProperty(libPrefix + "publickey", key, "string");
+}
+
+function loadKey(){
+  var publicKey = Bot.getProperty(libPrefix + "publickey");
+  var privateKey = Bot.getProperty(libPrefix + "privatekey");
+
+  if(!publicKey){ throw new Error("iPayments lib: no publicKey. You need to setup it") }
+  if(!privateKey){ throw new Error("iPayments lib: no privateKey. You need to setup it") }
+
+  return {
+    publicKey: publicKey,
+    privateKey: privateKey
+  }
 }
 
 on(libPrefix + 'Generatee', Generatee);
