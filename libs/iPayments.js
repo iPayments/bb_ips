413d9b9726626c60d66b43760abff59b40b3710d
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-08-01 14:08:58 +0530

    Update iPayments.js

--- libs/iPayments.js
+++ libs/iPayments.js
@@ -5,9 +5,19 @@ function GenerateAddress(){
   success: libPrefix + 'Generatee'
  })
 }
+
 function Generatee(){
    return Bot.sendMessage(""+content);
 }
+
+function setPrivateKey(key){
+Bot.setProperty(libPrefix + "privatekey", key, "string");
+}
+
+function setPublicKey(key){
+Bot.setProperty(libPrefix + "publickey", key, "string");
+}
+
 on(libPrefix + 'Generatee', Generatee);
 publish({
     GenerateAddress:GenerateAddress
