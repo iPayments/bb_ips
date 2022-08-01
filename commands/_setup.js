53acae03cc8a1205dc31df93466f3c6855338a20
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-08-01 14:02:37 +0530

    Update and rename setup.js to _setup.js

---
+++ commands/_setup.js
@@ -0,0 +1,5 @@
+let PrivateKey = ""
+let PublicKey = ""
+Libs.iPayments.setPrivateKey(""+PrivateKey+"");
+Libs.iPayments.setPublicKey(""+PublicKey+"");
+Bot.sendMessage("Done");
--- commands/setup.js
+++
@@ -1,2 +0,0 @@
-let PrivateKey = ""
-let PublicKey = ""
