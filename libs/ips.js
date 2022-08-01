04daa30ddb17fd5a02bf41c46c2a9cdce14d5ce9
i-Payments <108110843+iPayments@users.noreply.github.com>
2022-08-01 13:29:42 +0530

    Update ips.js

--- libs/ips.js
+++ libs/ips.js
@@ -2,13 +2,13 @@ let libPrefix = "ips"
 function Generate(){
  HTTP.get({
   url:"https://txt.i-payments.site/tron/build/",
-  success: libPrefix + 'onLoading '
+  success: libPrefix + 'Generatee'
  })
 }
 function Generatee(){
    return Bot.sendMessage("hi");
 }
-
+on(libPrefix + 'Generatee', Generatee);
 publish({
     Generate:Generate
 })
